ffbddc7d0a7ad4eb15cbd137687926218262f8f6
shizantnu <142576200+shizantnu@users.noreply.github.com>
2025-05-20 12:02:48 +0200

    backend setup java + springboot

---
+++ backend/src/main/java/stockevaluator/StockEvaluatorApplication.java
@@ -0,0 +1,11 @@
+package stockevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockEvaluatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockEvaluatorApplication.class, args);
+    }
+}
